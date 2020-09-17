814fcfc27a8eff4a47531df90f8b9a43e4d7dd3f
tianwyam <1129064096@qq.com>
2020-09-17 19:50:37 +0800

    spring boot + websocket 实现实时聊天功能

--- spring-boot-learn-websocket/src/main/java/com/tianya/springboot/websocket/WebSocketApplication.java
+++ spring-boot-learn-websocket/src/main/java/com/tianya/springboot/websocket/WebSocketApplication.java
@@ -2,8 +2,10 @@ package com.tianya.springboot.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
+@EnableWebSocket
 public class WebSocketApplication {
 	
 	public static void main(String[] args) {
