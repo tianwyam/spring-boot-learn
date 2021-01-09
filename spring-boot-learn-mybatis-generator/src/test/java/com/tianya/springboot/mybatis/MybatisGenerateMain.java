16efd5e0e7049b7f3a11c700394d96afa64227a8
tianwyam <1129064096@qq.com>
2021-01-09 14:07:27 +0800

    采用 java application应用方式 运行 mybatis generator

---
+++ spring-boot-learn-mybatis-generator/src/test/java/com/tianya/springboot/mybatis/MybatisGenerateMain.java
@@ -0,0 +1,41 @@
+package com.tianya.springboot.mybatis;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class MybatisGenerateMain {
+	
+	/**
+	 * @description
+	 *	运行 Mybatis Generate 
+	 *		方式一，采用main方法方式运行
+	 *		方式二，采用maven运行 mybatis-generator:generate
+	 * @author TianwYam
+	 * @date 2021年1月9日下午2:01:10
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		List<String> warnings = new ArrayList<>();
+		try (
+				InputStream inputStream = MybatisGenerateMain.class
+					.getClassLoader()
+					.getResourceAsStream("generatorConfig.xml"); ){
+			ConfigurationParser parser = new ConfigurationParser(warnings);
+			Configuration config = parser.parseConfiguration(inputStream);
+			DefaultShellCallback defaultShellCallback = new DefaultShellCallback(true);
+			MyBatisGenerator generator = new MyBatisGenerator(config, defaultShellCallback, warnings);
+			generator.generate(null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
