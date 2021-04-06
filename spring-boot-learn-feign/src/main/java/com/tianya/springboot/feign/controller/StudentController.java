a822848da17e6fa8462c90c1bf07d77393af71f3
tianwyam <1129064096@qq.com>
2021-04-06 22:19:43 +0800

    学习feign

--- spring-boot-learn-feign/src/main/java/com/tianya/springboot/feign/controller/StudentController.java
+++ spring-boot-learn-feign/src/main/java/com/tianya/springboot/feign/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.tianya.springboot.feign.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.tianya.springboot.common.entity.ResultUtis;
 import com.tianya.springboot.feign.client.StudentFeignClient;
@@ -15,10 +17,22 @@ public class StudentController {
 	@Autowired
 	private StudentFeignClient studentFeignClient ;
 
+	@Autowired
+	private RestTemplate restTemplate ;
+	
+	@Value("${student.feign.url}")
+	private String studentServerUrl ;
 	
 	@GetMapping("/list")
 	public ResultUtis getList() {
-		return studentFeignClient.getList();
+		
+		
+//		return studentFeignClient.getList();
+		
+
+		// 类似
+		return restTemplate.getForObject(studentServerUrl + "/list", ResultUtis.class);
+		
 	}
 	
 }
