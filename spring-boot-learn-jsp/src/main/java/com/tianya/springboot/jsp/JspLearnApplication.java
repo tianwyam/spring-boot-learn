e21b6438826907c8e9f61a4ebbbc247c37578604
tianwyam <1129064096@qq.com>
2021-04-29 23:01:23 +0800

    spring boot 集成 JSP

---
+++ spring-boot-learn-jsp/src/main/java/com/tianya/springboot/jsp/JspLearnApplication.java
@@ -0,0 +1,13 @@
+package com.tianya.springboot.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspLearnApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JspLearnApplication.class, args);
+	}
+
+}
