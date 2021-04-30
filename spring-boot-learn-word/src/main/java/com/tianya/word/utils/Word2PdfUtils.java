90e9449bc78f9ce47e69fefc6d737cc3b093e5d5
tianwyam <1129064096@qq.com>
2021-04-30 09:33:24 +0800

    java 对word文档添加水印

--- spring-boot-learn-word/src/main/java/com/tianya/word/utils/Word2PdfUtils.java
+++ spring-boot-learn-word/src/main/java/com/tianya/word/utils/Word2PdfUtils.java
@@ -1,5 +1,18 @@
 package com.tianya.word.utils;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFHeader;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.xmlbeans.XmlObject;
+
+import com.microsoft.schemas.vml.CTShape;
 import com.spire.doc.Document;
 import com.spire.doc.FileFormat;
 
@@ -27,11 +40,57 @@ public class Word2PdfUtils {
 	}
 	
 	
+	
+	/**
+	 * @description
+	 *	给word文档添加水印
+	 * @author TianwYam
+	 * @date 2021年4月29日下午6:01:13
+	 * @param wordFilePath
+	 * @param outputFilePath
+	 */
+	public static void addWaterMark(String wordFilePath, String outputFilePath) {
+
+		// XWPF 的 对word的操作 .docx
+		try (XWPFDocument document = new XWPFDocument(new FileInputStream(wordFilePath));) {
+			
+			XWPFHeaderFooterPolicy headerFooterPolicy = document.getHeaderFooterPolicy();
+			if (headerFooterPolicy == null) {
+				headerFooterPolicy = document.createHeaderFooterPolicy();
+			}
+
+			headerFooterPolicy.createWatermark("私人文件");
+			
+			XWPFHeader header = headerFooterPolicy.getHeader(XWPFHeaderFooterPolicy.DEFAULT);
+			List<XWPFParagraph> paragraphs = header.getParagraphs();
+			for (XWPFParagraph xwpfParagraph : paragraphs) {
+				
+				XmlObject[] xmlobjects = xwpfParagraph.getCTP().getRArray(0).getPictArray(0).selectChildren(
+						new QName("urn:schemas-microsoft-com:vml", "shape"));
+				if (xmlobjects.length > 0) {
+					CTShape ctshape = (CTShape) xmlobjects[0];
+					ctshape.setFillcolor("#d8d8d8");
+					ctshape.setStyle(ctshape.getStyle() + ";rotation:315");
+				}
+			}
+
+			document.write(new FileOutputStream(outputFilePath));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		
-		String wordFilePath = "M:\\test.doc" ;
-		String pdfFilePath = "M:\\w2p.pdf" ;
-		word2pdf(wordFilePath, pdfFilePath);
+		String wordFilePath = "M:\\wordTest.docx" ;
+//		String pdfFilePath = "M:\\w2p.pdf" ;
+//		word2pdf(wordFilePath, pdfFilePath);
+		
+		String outputFilePath = "M:\\wordTest22.docx" ;
+		addWaterMark(wordFilePath, outputFilePath);
 		
 	}
 	
