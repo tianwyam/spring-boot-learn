e1865ed0a483cff3c01c66d2734e30d9ea2ac972
tianwyam <1129064096@qq.com>
2021-04-30 16:28:25 +0800

    在resttemplate中设置 cookie 和 user-agent

--- spring-boot-learn-web/src/main/java/com/tianya/springboot/web/WebLearnApplication.java
+++ spring-boot-learn-web/src/main/java/com/tianya/springboot/web/WebLearnApplication.java
@@ -3,7 +3,9 @@ package com.tianya.springboot.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class WebLearnApplication {
 
