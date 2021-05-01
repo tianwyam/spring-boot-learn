ef63eb1d5263b744392f4f3bd9a679bde074aab0
tianwyam <1129064096@qq.com>
2021-05-01 11:04:31 +0800

    学习free marker

---
+++ spring-boot-learn-freemarker/src/main/java/com/tianya/springboot/freemarker/LearnFreemarkerApplication.java
@@ -0,0 +1,13 @@
+package com.tianya.springboot.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFreemarkerApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(LearnFreemarkerApplication.class, args);
+	}
+
+}
