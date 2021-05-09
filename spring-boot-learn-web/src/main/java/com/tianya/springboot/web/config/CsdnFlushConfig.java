aee5903a743be2f1e6374f0f4006c28b7ad1c8f7
tianwyam <1129064096@qq.com>
2021-05-09 12:33:57 +0800

    update

--- spring-boot-learn-web/src/main/java/com/tianya/springboot/web/config/CsdnFlushConfig.java
+++ spring-boot-learn-web/src/main/java/com/tianya/springboot/web/config/CsdnFlushConfig.java
@@ -1,5 +1,8 @@
 package com.tianya.springboot.web.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpEntity;
@@ -27,20 +30,30 @@ public class CsdnFlushConfig {
 		headers.set("sec-fetch-site", "same-origin");
 		headers.set("sec-fetch-user", "?1");
 		headers.set("upgrade-insecure-requests", "1");
-		headers.set("referer", "https://blog.csdn.net/mybook201314?spm=1010.2135.3001.5343");
+		headers.set("referer", "https://blog.csdn.net/mybook201314");
 		headers.set("authority", "blog.csdn.net");
 		headers.set("method", "GET");
-		headers.set("path", "/mybook201314/article/details/116398838");
 		headers.set("scheme", "https");
 		headers.set("accept-encoding", "gzip, deflate, br");
 		headers.set("cache-control", "max-age=0");
 		
 		
-		HttpEntity<String> entity = new HttpEntity<>(null, headers);
-
-
-		String url = "https://blog.csdn.net/mybook201314/article/details/116398838";
-		restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+		
+		String csdnUrl = "https://blog.csdn.net" ;
+		List<String> urlList = Arrays.asList(
+				"/mybook201314/article/details/102641119",
+				"/mybook201314/article/details/115336106",
+				"/mybook201314/article/details/116398838",
+				"/mybook201314/article/details/115363090",
+				"/mybook201314/article/details/116277989",
+				"/mybook201314/article/details/115711242",
+				"/mybook201314/article/details/116422683" );
+		for (String url : urlList) {
+			headers.set("path", url);
+			HttpEntity<String> entity = new HttpEntity<>(null, headers);
+			restTemplate.exchange( csdnUrl + url, HttpMethod.GET, entity, String.class);
+		}
+		
 		
 		System.out.println("刷新CSDN阅读量 成功 ");
 	}
