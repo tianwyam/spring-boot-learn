4e36ed0532bd11c4285aa18d8039623c0b0ac211
tianwyam <1129064096@qq.com>
2021-06-22 21:44:33 +0800

    使用javafx画个GUI

--- spring-boot-learn-javafx/src/main/java/com/tianya/springboot/javafx/controller/MainStageController.java
+++ spring-boot-learn-javafx/src/main/java/com/tianya/springboot/javafx/controller/MainStageController.java
@@ -4,11 +4,75 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import de.felixroske.jfxsupport.FXMLController;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 @FXMLController
-@SuppressWarnings("restriction")
+@SuppressWarnings("all")
 public class MainStageController implements Initializable {
+	
+	
+	/**
+	 * maven 本地安装地址
+	 */
+	@FXML
+	private TextField mavenHome ;
+	
+	
+	/**
+	 * maven 远程仓库地址
+	 */
+	@FXML
+	private TextField mavenRemoteRepoAddr ;
+	
+	/**
+	 * maven 远程仓库 user
+	 */
+	@FXML
+	private TextField mavenRemoteRepoUser ;
+	
+	/**
+	 * maven 远程仓库 password
+	 */
+	@FXML
+	private TextField mavenRemoteRepoPassword ;
+	
+	
+	/**
+	 * maven 本地仓库地址
+	 */
+	@FXML
+	private TextField mavenLocalRepoAddr ;
+	
+	/**
+	 * maven 上传进程
+	 */
+	@FXML
+	private TextArea mavenUploadProcess ;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void uploadJar2Remote() {
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
 
     public void initialize(URL location, ResourceBundle resources) {
 
